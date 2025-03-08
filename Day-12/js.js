4db70fa36665221ff6d7b19ec938be79d2cf0c20
Nguyhaso <94haison@gmail.com>
2025-03-08 13:34:07 +0700

    gui bai tap

---
+++ Day-12/js.js
@@ -0,0 +1,48 @@
+// Bài 1 số nguyên tố
+// var x = 1231 // check if x is prime number?
+// var j = 0
+// if (x === 2 || x === 3) {
+//     console.log("x la so nguyen to")
+// } else {
+//     for (var i = 2; i <= 3; i++) {
+//         if (x % i === 0) {
+//             j = j + 1
+//         }
+//     }
+//     if (j > 0 ) {
+//         console.log('x khong phai so nguyen to')
+//     }
+//     else {
+//         console.log("x la so nguyen to")
+//
+//     }
+// }
+// Bài 2 số chính phương
+// var y = 121 // check if x is square number ?
+// var z = 0
+// for (var g=1; g <= y**0.5; g++){
+//     if (g**2 === y){
+//         z = 1
+//     }
+// }
+// if (z === 0){
+//     console.log(y, ' la khong phai so chinh phuong')
+// }
+// else {
+//     console.log(y, ' la so chinh phuong')
+//
+// }
+// Bài 3 số hoàn hảo
+// var x = 28 // check if x is perfect number?
+// var sum = 0
+// for (var i = 1; i <= x/2; i ++){
+//     if (x%i ===0){
+//         sum= sum + i
+//     }
+// }
+// if (sum === x){
+//     console.log( x, 'la so hoan hao')
+// }
+// else{
+//     console.log( x, 'khong phai so hoan hao')
+// }
